e5cd455e6e3ed77537cc032ff95ae96ebc9367db
Ubuntu <dhgrainger@gmail.com>
2016-09-12 21:02:39 +0000

    more staging configs

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,3 +1,5 @@
-config.cache_classes = true
-config.action_controller.perform_caching = true
-config.action_view.cache_template_loading = true
+environment_configuration(defined?(config) && config) do |config|
+  config.cache_classes = true
+  config.action_controller.perform_caching = true
+  config.action_view.cache_template_loading = true
+end

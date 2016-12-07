297529c045b6af6b81a433eb5d5eff316fea17af
John Naegle <john.naegle@goodmeasures.com>
2016-12-07 14:25:54 -0600

    Eager load in the staging enviornment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,4 +2,5 @@ environment_configuration(defined?(config) && config) do |config|
   config.cache_classes = true
   config.action_controller.perform_caching = true
   config.action_view.cache_template_loading = true
+  config.eager_load = true
 end
